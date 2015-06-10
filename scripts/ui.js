d940a5266143e595194979d40adf8789e56d6cb7
Tran TRI DUE <dtrantri@pentalog.fr>
2015-06-10 18:23:19 +0700

    commit

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,4 +1,4 @@
-var delaytime = 10;
+var delaytime = 500;
 // GO TO DIRECTLY AN URL
 function goToPage(link) {
 	window.location.href = "login-home.php?module=" + link;
@@ -13,9 +13,9 @@ $(function() {
 });
 $(function() {
 $(".datetimefield").datetimepicker({
-	formatTime:'H:i',
+	formatTime:'H:i:s',
 	formatDate:'d.m.Y',
-	//defaultDate:'8.12.1986', // it's my birthday
+//	defaultDate:'8.12.1986', // it's my birthday
 //	defaultDate:'+03.01.1970', // it's my birthday
 //	defaultTime:'10:00',
 	timepickerScrollbar:true
