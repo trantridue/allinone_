0d622304a62d49f27cf779f8990df565b6b8be0d
Tran TRI DUE <dtrantri@pentalog.fr>
2015-06-17 08:33:27 +0700

    a

--- modules/export/marquee.php
+++ modules/export/marquee.php
@@ -1,6 +1,6 @@
 <marquee onmouseout="this.setAttribute('scrollamount', 1 , 0);" direction="up"
 	onmouseover="this.setAttribute('scrollamount', 0, 0);" behavior="scroll"
-	scrollamount="1" loop="-1" style="font-size:16px;margin-top:5px;">
+	scrollamount="1" loop="-1" style="margin-top:5px;">
 	<?php 
 		$newsService->latestNews();
 	?>
