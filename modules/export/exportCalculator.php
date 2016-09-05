edfdeca870e4edc8e53c016e7afca1ee851d07fc
Tran TRI DUE <dtrantri@pentalog.fr>
2016-09-05 09:12:43 +0700

    update calculator

--- modules/export/exportCalculator.php
+++ modules/export/exportCalculator.php
@@ -1,30 +1,3 @@
-	    <span style="padding-left:30%" >
-<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="262" height="322" id="CalculatorID">
-        <param name="movie" value="http://www.embedcalculator.com/flash/StandardCalculator.swf" />
-        <param name="quality" value="high" />
-        <param name="wmode" value="opaque" />
-        <param name="swfversion" value="9.0.45.0" />
-        <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don't want users to see the prompt. -->
-        <param name="expressinstall" value="http://www.embedcalculator.com/Scripts/expressInstall.swf" />
-        <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
-        <!--[if !IE]>-->
-        <object type="application/x-shockwave-flash" data="http://www.embedcalculator.com/flash/StandardCalculator.swf" width="262" height="322">
-          <!--<![endif]-->
-          <param name="quality" value="high" />
-          <param name="wmode" value="opaque" />
-          <param name="swfversion" value="9.0.45.0" />
-          <param name="expressinstall" value="http://www.embedcalculator.com/Scripts/expressInstall.swf" />
-          <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
-          <div>
-            <h4><a href="http://www.embedcalculator.com">Embed Calculator</a> requires a newer version of Adobe Flash Player.</h4>
-            <p>You must have the current version of the adobe flash player to use this <a href="http://www.embedcalculator.com">online calculator</a>.</p>
-          </div>
-          <!--[if !IE]>-->
-        </object>
-        <!--<![endif]-->
-      </object>
-            </span>
-			<script type="text/javascript">
-            swfobject.registerObject("CalculatorID");
-            </script>
-    
+<iframe width="320" height="420" 
+src="http://web2.0calc.com/widgets/minimal/?options=%7B%22angular%22%3A%22deg%22%2C%22options%22%3A%22hide%22%2C%22menu%22%3A%22show%22%7D" scrolling="no" style="border: 1px solid silver; "> 
+</iframe>
