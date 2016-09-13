743c836ebeccb9dd58b756727412b409a3f25464
Tran TRI DUE <dtrantri@pentalog.fr>
2016-09-13 21:20:32 +0700

    add export view mobile

--- common/header_admin.php
+++ common/header_admin.php
@@ -1,8 +1,9 @@
 <td <?php $commonService->displayColor('#F5FFC4','black');?>>EX : <?php echo $reportService->getAmountToDay();?></td>
-<td <?php $commonService->displayColor('#E20C14','white');?>>ETD : <?php echo $reportService->getInteretByShopAndDate($td,$td,'all');?></td>
+<td <?php $commonService->displayColor('#E20C14','white');?>>ETD : <?php $t=$reportService->getAmountToDay(); echo $t; ?></td>
 <td <?php $commonService->displayColor('#F5FFC4','black');?>>EMO : <?php echo $reportService->getInteretByShopAndDate(date('Y-m-01'),date('Y-m-t'),'all');?></td>
 <td <?php $commonService->displayColor('#E20C14','white');?>>RE : <?php echo $reportService->getReturnByShopAndDate($td,$td,'all');?></td>
 <td <?php $commonService->displayColor('#F5FFC4','black');?>>ROI : <?php echo $reportService->getRoiByShopAndDate($td,$td,'all');?></td>
 <td <?php $commonService->displayColor('#E20C14','white');?>>CA : <?php echo $reportService->getCashByShop($td,$td,'all');?></td>
 <td <?php $commonService->displayColor('#F5FFC4','black');?>>KET : <?php echo number_format($reportService->amountInket(),0);?></td>
 <td <?php $commonService->displayColor('#E20C14','white');?>>INOUT : <?php echo $reportService->getInoutByShopAndDate($td,$td,'all')?></td>
+<?php if ($commonService->isMobile ()) {?> <span class="exportclass"> <?php echo $t;?> </span> <?php }?>
