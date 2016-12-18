87a9b579f49d60e83a6a68c6b8a058cd3a9c0602
Tran TRI DUE <dtrantri@pentalog.fr>
2016-12-18 20:53:40 +0700

    a

---
+++ database/update_duplicated_customer.sql
@@ -0,0 +1,5 @@
+﻿update export_facture set customer_id = 6698 where customer_id = 3641;
+update customer_reservation_histo set customer_id = 6698 where customer_id = 3641;
+update export_facture_trace set customer_id = 6698 where customer_id = 3641;
+
+update customer set name = 'Chi Hoang', tel = '0945xxxxxx' where id = 3641;
