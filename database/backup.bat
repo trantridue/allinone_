4e92677d6c41c91283b3157780f2684657c5f376
Tran TRI DUE <dtrantri@pentalog.fr>
2017-02-18 12:53:28 +0700

    a

--- database/backup.bat
+++ database/backup.bat
@@ -4,5 +4,5 @@ pscp -pw RAUE9465jkwh root@103.1.236.134:/root/zkpmolfu_banhang.sql C:\AppServ\w
 cd C:\AppServ\www\allinone\database
 "C:\Program Files\Git\bin\git.exe" pull
 "C:\Program Files\Git\bin\git.exe" add C:\AppServ\www\allinone\database\zkpmolfu_banhang.sql
-"C:\Program Files\Git\bin\git.exe" commit -m "auto backup database"
+"C:\Program Files\Git\bin\git.exe" commit -m "auto backup database from shop 1"
 "C:\Program Files\Git\bin\git.exe" push
