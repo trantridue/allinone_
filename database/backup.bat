9c92852eb06235069d15c937b6fb7a3dfeb5fba7
Tran TRI DUE <dtrantri@pentalog.fr>
2017-02-18 09:16:57 +0700

    auto backup

--- database/backup.bat
+++ database/backup.bat
@@ -1,4 +1,5 @@
 mysqldump --single-transaction -hlocalhost -utest -ptest zkpmolfu_banhang > zkpmolfu_banhang.sql
+cd C:\xampp\htdocs\allinone\database
 git add --all
 git commit -m "auto backup"
 git push
