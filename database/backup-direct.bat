6f9df35925c73cd20e544e37e71b749f9d6b7593
Tran TRI DUE <dtrantri@pentalog.fr>
2017-02-18 09:55:29 +0700

    a

--- database/backup-direct.bat
+++ database/backup-direct.bat
@@ -2,6 +2,7 @@ cd C:\xampp\htdocs\allinone\database
 plink -ssh -pw RAUE9465jkwh root@103.1.236.134 "sh /root/backup.sh"
 pscp -pw RAUE9465jkwh root@103.1.236.134:/root/zkpmolfu_banhang.sql C:\xampp\htdocs\allinone\database\
 cd C:\xampp\htdocs\allinone\database
-git add --all
-git commit -m "auto backup"
+git pull
+git add C:\xampp\htdocs\allinone\database\zkpmolfu_banhang.sql
+git commit -m "auto backup database"
 git push
