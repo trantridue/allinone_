a6ffa861cefaf20e38e2cde025ee0f8494481a80
Tran TRI DUE <dtrantri@pentalog.fr>
2017-03-22 21:55:25 +0700

    a

--- database/backup-direct.bat
+++ database/backup-direct.bat
@@ -1,9 +1,10 @@
-cd C:\xampp\htdocs\allinone\database
+D:
+cd D:\Projects\OTS\booking-room-project\web\allinone\database
 git pull
 plink -ssh -pw RAUE9465jkwh root@103.1.236.134 "sh /root/backup.sh"
-pscp -pw RAUE9465jkwh root@103.1.236.134:/root/zkpmolfu_banhang.sql C:\xampp\htdocs\allinone\database\
-cd C:\xampp\htdocs\allinone\database
+pscp -pw RAUE9465jkwh root@103.1.236.134:/root/zkpmolfu_banhang.sql D:\Projects\OTS\booking-room-project\web\allinone\database\
+cd D:\Projects\OTS\booking-room-project\web\allinone\database
 git pull
-git add C:\xampp\htdocs\allinone\database\zkpmolfu_banhang.sql
+git add D:\Projects\OTS\booking-room-project\web\allinone\database\zkpmolfu_banhang.sql
 git commit -m "auto backup database from laptop"
 git push
