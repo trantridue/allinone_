850444088aeda41ed115cda2e3c9832effad9512
Tran TRI DUE <dtrantri@pentalog.fr>
2017-03-29 23:25:11 +0700

    update script backup booking only

--- database/backup-booking.bat
+++ database/backup-booking.bat
@@ -1,7 +1,7 @@
 D:
 cd D:\Projects\OTS\booking-room-project\web\allinone\database
 git pull
-plink -ssh -pw RAUE9465jkwh root@103.1.236.134 "sh /root/backup.sh"
+plink -ssh -pw RAUE9465jkwh root@103.1.236.134 "sh /root/backup_booking.sh"
 pscp -pw RAUE9465jkwh root@103.1.236.134:/root/bookingroom.sql D:\Projects\OTS\booking-room-project\web\allinone\database\
 cd D:\Projects\OTS\booking-room-project\web\allinone\database
 git pull
