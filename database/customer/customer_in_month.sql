089a3f2ff0d4b8e4c8fbc3637625b8e68bb91562
Tran TRI DUE <dtrantri@pentalog.fr>
2017-03-31 23:28:42 +0700

    a

--- database/customer/customer_in_month.sql
+++ database/customer/customer_in_month.sql
@@ -1,4 +1,15 @@
-select us.id,us.name,us.tel as tel, sum((efp.quantity) * efp.export_price) as total, ef.date
-from customer us,export_facture_product efp, export_facture ef
-where us.id = ef.customer_id and DATE_FORMAT(ef.date, '%Y-%m') = DATE_FORMAT(now(), '%Y-%m') and
- efp.export_facture_code = ef.code group by us.tel order by total desc;
+﻿SELECT us.id,
+       us.NAME, 
+       us.tel                                   AS tel, 
+       Sum(( efp.quantity ) * efp.export_price) AS total, 
+       ef.code, 
+       ef.date 
+FROM   customer us, 
+       export_facture_product efp, 
+       export_facture ef 
+WHERE  us.id = ef.customer_id 
+       AND Date_format(ef.date, '%Y-%m') = Date_format(Now(), '%Y-%m') 
+       AND efp.export_facture_code = ef.code 
+       AND us.tel NOT LIKE '%aaa%' 
+GROUP  BY ef.code 
+ORDER  BY us.tel DESC; 
